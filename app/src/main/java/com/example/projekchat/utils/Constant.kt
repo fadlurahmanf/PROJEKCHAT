6c653135903f944835c00cf9ebda50324b04efb3
taufik <tffajari@gmail.com>
2021-06-27 12:12:09 +0700

    INITIAL COMMIT

--- app/src/main/java/com/example/projekchat/utils/Constant.kt
+++ app/src/main/java/com/example/projekchat/utils/Constant.kt
@@ -1,7 +1,6 @@
 package com.example.projekchat.utils
 
 object Constant {
-    const val SERVER_KEY = "AAAAkM05kUY:APA91bHBU6dn0x4bsacx5-kLio9YMvBrGOySP4fOvSpJgVvwcqDqqKfL7mtvj8sDwPpgSUe7Grhu1GAn2tJytV_Asc9tC7DRxQpADbGIQiji8qSdJQ7kf-YMWP23VT0BZjmo6esTSLwe"
     const val NOTIFICATION_URL = "https://fcm.googleapis.com/fcm/send"
     const val CHANNEL_ID = "Message Channel"
 }
