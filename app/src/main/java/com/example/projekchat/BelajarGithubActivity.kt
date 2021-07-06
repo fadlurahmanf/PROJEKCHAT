ff36619a3a11f0acf24d43f59dbfb9f8a4ad4b74
taufik <tffajari@gmail.com>
2021-07-06 19:38:47 +0700

    PENAMBAHAN DARI SISI ANDROID STUDIO

---
+++ app/src/main/java/com/example/projekchat/BelajarGithubActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projekchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BelajarGithubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_belajar_github)
+    }
+}
--- app/src/main/java/com/example/projekchat/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/projekchat/ui/home/HomeActivity.kt
@@ -19,7 +19,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
-//PENAMBAHAN PADA HOME ACTIVITY
+//PENAMBAHAN PADA HOME ACTIVITY DARI SISI GITHUB
 
 class HomeActivity : AppCompatActivity() {
 
