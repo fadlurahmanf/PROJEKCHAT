38778f8f88f5c00db5fe8d697ab36eb9a6ac3a69
taufik <tffajari@gmail.com>
2021-07-06 19:30:34 +0700

    INITIAL COMMIT

--- app/src/main/java/com/example/projekchat/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/projekchat/ui/home/HomeActivity.kt
@@ -19,7 +19,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
-//PENAMBAHAN
+//PENAMBAHAN PADA HOME ACTIVITY
 
 class HomeActivity : AppCompatActivity() {
 
