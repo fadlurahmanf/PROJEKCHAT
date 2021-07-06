cfb8fa864e15a0d3700efdc34bd6d7533b905e04
taufik <tffajari@gmail.com>
2021-07-06 19:44:59 +0700

    INITIAL COMMIT

--- app/src/main/java/com/example/projekchat/BelajarGithubActivity.kt
+++ app/src/main/java/com/example/projekchat/BelajarGithubActivity.kt
@@ -3,6 +3,9 @@ package com.example.projekchat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+//PENAMBAHAN STANDARD
+
 class BelajarGithubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
